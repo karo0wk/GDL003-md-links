5d9b609d520d487c90bda3efc273621eee360b42
GalioV <karo0wk@gmail.com>
2019-08-19 18:07:40 -0500

    primera funcion testeada

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-  // ...
+
+const path = require('path');
+
+// const liga = RegExp("http*");
+
+module.exports = (filePath) => {
+if (path.extname(filePath)=== '.md'){
+    return true;
+}
+else{
+    return false;
+}
 };
+
+
+// module.exports = (fileUrl) => liga.text(fileUrl)=== 'http';
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -3,8 +3,17 @@ const mdLinks = require('../');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should return the for a valid .md file', () => {
+    expect(mdLinks('../README.md')).toBe(true);
   });
 
 });
+/*
+describe('mdLinks', () => {
+
+  it('should return a valid http file', () => {
+    expect(mdLinks('../README.md')).toBe(true);
+  });
+
+});
+*/
