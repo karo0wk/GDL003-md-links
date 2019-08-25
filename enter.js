780fe40c39449e42f84e80ceeacff2d94fe61f9e
GalioV <karo0wk@gmail.com>
2019-08-25 00:38:29 -0500

    Problemas de sintaxis solucionados en la segunda funcion

--- enter.js
+++ enter.js
@@ -1,6 +1,39 @@
-const index = require("./index");
+
+const path = require('path');
+var fs = require("fs");
+
+module.exports = { 
 
 
-//console.log(index.extArch("./README.md"));
-let file= index.existentFile("./README.md")
- console.log(file);
+extArch : (filePath)=> {
+  return path.extname(filePath)===".md";
+  },
+
+
+existentFile: () => 
+    {
+      fs.stat('index.js', function(err) {
+        if (err == null) {
+          console.log("El archivo existe");
+        } else if (err.code == 'ENOENT') {
+          console.log("el archivo no existe");
+        } else {
+          console.log(err); // ocurrió algún error
+        }
+      });
+    },
+
+
+
+  seeFile: (fileP, callback ) =>
+    {
+      fs.readFile(fileP, (error, data)=>{
+        if(error){
+          throw error;
+        }
+        callback(data);
+      });
+    }
+
+
+};
--- index.js
+++ index.js
@@ -1,41 +1,9 @@
+const index = require("./enter");
 
-const path = require('path');
-var fs = require("fs");
-
-// const liga = RegExp("http*");
-
-module.exports = { 
-extArch: (filePath) => 
-
-      {
-    if (path.extname(filePath)=== '.md'){
-        return true;
-      }
-    else
-    {
-        return false;
-    }
-      }
-};
+console.log(index.extArch("./README.js"));
+console.log(index.existentFile());
+console.log(index.seeFile("./readme.md", (data)=>{console.log(data)}));
 
 
-
-module.exports = {
-  existentFile: () => 
-    {
-      fs.stat('README.md', function(err) {
-        if (err == null) {
-          console.log("El archivo existe");
-        } else if (err.code == 'ENOENT') {
-          console.log("el archivo no existe");
-        } else {
-          console.log(err); // ocurrió algún error
-        }
-      });
-    }
-}
-
-
-
-
-// module.exports = (fileUrl) => liga.text(fileUrl)=== 'http';
+//  let see = index.seeFile('/carmaba.txt');
+//  console.log(see)
